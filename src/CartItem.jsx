ff5f49d7ae66a3230eff70666b9f95256d78edcb
ray foster <ray.foster@zestplan.com>
2026-05-06 04:42:58 -0400

    cart calcs

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,44 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(function(item){
+        const {quantity, cost } = item;
+        const parsedPrice = parseFloat(cost.substring(1));
+        const price = quantity * parsedPrice;
+        total += price;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Coming soon! Functionality to be added in the future');
+  };
+  
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name:item.name, quantity:item.quantity+1}))
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity>1){
+        dispatch(updateQuantity({name:item.name, quantity:item.quantity-1}))
+    }else{
+        dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * parseFloat(item.cost.substring(1));
   };
 
   return (
